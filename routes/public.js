dc6557a33d00163a7bfe8a27d924c3451ab008f3
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2025-06-07 13:47:17 -0300

    Initial commit

---
+++ routes/public.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const router = express.Router()
+
+//Cadastro
+router.post('/cadastro', async (req, res) => {
+    try{
+    const user = req.body
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
+
+    const userDB = await prisma.user.create({
+        data: {
+            email: user.email, 
+            name: user.name, 
+            password: hashPassword,
+        },
+    })
+    res.status(201).json(userDB)
+    } catch (err) {
+        res.status(500).json({message: "Erro no servidor"})
+    }
+})
+
+
+
+export default router
+
+//bugmaer
+//98143360
+//mongodb+srv://<db_username>:<db_password>@cluster0.rr3f2my.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
