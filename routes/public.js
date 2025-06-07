8e1a4f7fda6a1578f40f9101e91ef167d7073c1b
César Bugmaer <162216315+Bugmaer@users.noreply.github.com>
2025-06-07 14:12:09 -0300

    Criação do token JWT

--- routes/public.js
+++ routes/public.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
 
+const JWT_SECRET = process.env.JWT_SECRET
 const prisma = new PrismaClient()
 const router = express.Router()
 
@@ -31,23 +33,27 @@ router.post('/cadastro', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {    
     const userInfo = req.body
-
-
+    
+    //Buscando usuário
     const user = await prisma.user.findUnique({     
         where: { email: userInfo.email },
         })
 
+        //Verificando
         if(!user){
             return res.status(404).json({message: 'Usuário não encontrado'})
         }
 
-        const isMatch = await bcrypt.compare(userInfo.password, user.Password)
+        // Comparando
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
 
         if(!isMatch){
-            return res.status(400).json({message: 'Senha inválida'})
-        }
+        return res.status(401).json({message: 'Senha inválida'})
+}
+        //Gerando o token JWT
+        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '1m'})
 
-        res.status(200).json(user)
+        res.status(200).json(token)
     } catch(err){
         res.status(500).json({message: "Erro no servidor"})
     }
